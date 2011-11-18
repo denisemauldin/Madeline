98ad25537bfbdf2525dfbdd7c0314bb9ff0df67f
Denise Mauldin <dmauldin@bobama.systemsbiology.net>
2011-11-18 13:35:15 -0800

    updated to new madeline version, bumped gem version

--- lib/madeline.rb
+++ lib/madeline.rb
@@ -1,12 +1,12 @@
 module Madeline
 
-  VERSION 		= "0.1.7"
+  VERSION 		= "0.1.8"
 
   MADELINE_VERSION	= "2.0"
 
   MADELINE_ROOT		= File.expand_path(File.dirname(__FILE__))
 
-  MADELINE_COMMAND	= "#{MADELINE_ROOT}/../ext/madeline/madeline2-linux-x86_64-r97"
+  MADELINE_COMMAND	= "#{MADELINE_ROOT}/../ext/madeline/madeline2-linux-x86_64-r98"
 
 
 end
